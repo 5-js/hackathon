2943fd533d421dc32bf00dce1bdb8743c31e1bc1
Jio Mamaoag <jiomamaoag@gmail.com>
2018-10-20 20:19:24 +0800

    Add Role Column

--- web/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('avatar')->nullable();
             $table->string('password');
+            $table->integer('role');
             $table->string('mobile');
             $table->rememberToken();
             $table->timestamps();
