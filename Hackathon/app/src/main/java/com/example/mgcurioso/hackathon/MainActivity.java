1142e8fe7f880859c7897e148b0a927e98b24330
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-10-21 12:48:53 +0800

    Fix error

--- Hackathon/app/src/main/java/com/example/mgcurioso/hackathon/MainActivity.java
+++ Hackathon/app/src/main/java/com/example/mgcurioso/hackathon/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
