d8194278ee3591c41585873c1e0914537312537e
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2018-10-21 12:38:46 +0800

    Add Task Connectivity

--- Hackathon/app/src/main/java/com/example/mgcurioso/hackathon/config/UrlsList.java
+++ Hackathon/app/src/main/java/com/example/mgcurioso/hackathon/config/UrlsList.java
@@ -3,7 +3,7 @@ package com.example.mgcurioso.hackathon.config;
 public final class UrlsList {
     public static final String BASE_URL = AppConfig.PRODUCTION
             ? "to be set"
-            : "http://http://172.16.130.71:8000/";
+            : "http://172.16.130.71:8000/";
     public static final String API_URL = BASE_URL + "api/";
     public static final String TASKS_URL = API_URL + "tasks/";
 }
